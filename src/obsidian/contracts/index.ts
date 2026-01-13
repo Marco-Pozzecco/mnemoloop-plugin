91287884f585f3135de0031e10d23beef65b9087
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-01-13 10:45:36 +0100

    feat: add Obsidian contract interfaces

---
+++ src/obsidian/contracts/index.ts
@@ -0,0 +1,25 @@
+export type {
+  IVaultEvent,
+  IVaultWatcherConfig,
+  IVaultWatcher
+} from './IVaultWatcher';
+
+export type {
+  EventProcessor,
+  IEventQueueConfig,
+  IQueueStatus,
+  IEventQueue
+} from './IEventQueue';
+
+export type {
+  IPluginSettings,
+  ISettingsManager
+} from './ISettingsManager';
+
+export type {
+  ICommandDefinition,
+  CommandResult,
+  ICommandRegistry
+} from './ICommandRegistry';
+
+export { VaultEventType } from './IVaultWatcher';
