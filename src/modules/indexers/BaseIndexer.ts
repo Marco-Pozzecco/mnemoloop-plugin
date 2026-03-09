424a4163790fb867cde776fba153a7676c58b7af
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-09 16:58:39 +0100

    feat: implment BaseIndexer class that implements IIndexer interface

---
+++ src/modules/indexers/BaseIndexer.ts
@@ -0,0 +1,85 @@
+import type { IAdapter } from '@/interfaces/IAdapter';
+import { IIndexer } from '@/interfaces/IIndexer';
+import { IParser } from '@/interfaces/IParser';
+import { PluginSettings } from '@/schemas/settings';
+import { Cache } from '@/utils/Cache';
+
+export abstract class BaseIndexer<Entity extends EntityYaml, EntityYaml, Index> implements IIndexer<Entity> {
+  protected _cache: Cache<Entity> = new Cache();
+  protected _parser: IParser<Entity, EntityYaml>;
+  protected _settings: IAdapter<PluginSettings>;
+  protected _adapter: IAdapter<Index>;
+
+  constructor(parser: IParser<Entity, EntityYaml>, settings: IAdapter<PluginSettings>, adapter: IAdapter<Index>) {
+    this._parser = parser;
+    this._settings = settings;
+    this._adapter = adapter;
+  }
+
+  get index(): Record<string, Entity> {
+    return this._cache.dump();
+  }
+
+  abstract initialize: () => Promise<void>;
+
+  abstract save: () => Promise<void>;
+
+  get: (id: string) => Entity | undefined = (id) => {
+    return this._cache.get(id);
+  };
+
+  getAll: () => Entity[] = () => {
+    return this._cache.getAll();
+  };
+
+  query: (predicate: (entity: Entity) => boolean) => Entity[] = (predicate) => {
+    return this._cache.query(predicate)
+  };
+
+  create: (id: string, data: Entity) => Entity = (id, data) => {
+    this._cache.set(id, data);
+    const entity = this._cache.get(id);
+    if (!entity) {
+      throw new Error(IndexError.FAILED_TO_CREATE);
+    }
+
+    return entity;
+  };
+
+  update: (id: string, data: Partial<Entity>) => Entity = (id, data) => {
+    const entity = this._cache.get(id);
+
+    if (!entity) {
+      throw new Error(IndexError.NOT_FOUND);
+    }
+
+    const updatedEntity = {
+      ...entity,
+      ...data
+    }
+
+    this._cache.set(id, updatedEntity);
+
+    const result = this._cache.get(id);
+
+    if (!result) {
+      throw new Error(IndexError.FAILED_TO_UPDATE);
+    }
+
+    return result;
+  };
+
+  delete: (id: string) => void = (id) => {
+    const has = this._cache.has(id);
+    if (!has) throw new Error(IndexError.NOT_FOUND);
+    return this._cache.delete(id);
+  };
+}
+
+enum IndexError {
+  NOT_FOUND = "entity not found in index",
+  FAILED_TO_CREATE = "failed to create entity in index",
+  FAILED_TO_DELETE = "failed to delete entity in index",
+  FAILED_TO_UPDATE = "failed to update entity in index"
+}
+
