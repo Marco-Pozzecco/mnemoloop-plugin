3eed3eb8d382eae9786e7e6bf59eb119bac4e107
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-10 14:17:02 +0100

    refactor: improve code format

--- src/modules/yaml-engines/FlashcardYamlEngine.ts
+++ src/modules/yaml-engines/FlashcardYamlEngine.ts
@@ -1,11 +1,11 @@
 import { IYamlEngine } from "@/interfaces/IYamlEngine";
 import { FlashcardMetadata, FlashcardMetadataSchema } from "@/schemas";
 import { BaseYamlEngine } from "./BaseYamlEngine";
-import { VaultAdapter } from "../obsidian";
+import { Plugin } from "obsidian";
 
 export class FlashcardYamlEngine extends BaseYamlEngine<FlashcardMetadata> implements IYamlEngine<FlashcardMetadata> {
-  constructor(vaultAdapter: VaultAdapter) {
-    super(vaultAdapter, FlashcardMetadataSchema)
+  constructor(plugin: Plugin) {
+    super(plugin, FlashcardMetadataSchema)
   }
 }
 
