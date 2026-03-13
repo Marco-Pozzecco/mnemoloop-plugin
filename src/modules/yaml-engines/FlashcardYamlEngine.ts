77d17112dde83f0a493c9fa508bc5d02e8ec38fe
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-13 16:06:38 +0100

    feat: implement recover method using default metadata settings.

--- src/modules/yaml-engines/FlashcardYamlEngine.ts
+++ src/modules/yaml-engines/FlashcardYamlEngine.ts
@@ -1,11 +1,25 @@
 import { IYamlEngine } from "@/interfaces/IYamlEngine";
-import { FlashcardMetadata, FlashcardMetadataSchema } from "@/schemas";
+import { DEFAULT_FLASHCARD_METADATA, FlashcardMetadata, FlashcardMetadataSchema } from "@/schemas";
 import { BaseYamlEngine } from "./BaseYamlEngine";
 import { Plugin } from "obsidian";
+import { v4 as uuid } from "uuid";
 
 export class FlashcardYamlEngine extends BaseYamlEngine<FlashcardMetadata> implements IYamlEngine<FlashcardMetadata> {
   constructor(plugin: Plugin) {
     super(plugin, FlashcardMetadataSchema)
+  }
+
+  recover: (filepath: string) => Promise<void> = async (filepath) => {
+    await this.write(filepath, this.generateDefaultYaml(filepath));
+  };
+
+  private generateDefaultYaml(filepath: string) {
+    const yaml: FlashcardMetadata = {
+      uuid: uuid(),
+      file: filepath,
+      ...DEFAULT_FLASHCARD_METADATA,
+    }
+    return yaml;
   }
 }
 
