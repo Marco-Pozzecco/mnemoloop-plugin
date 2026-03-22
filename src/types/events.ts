de0ebd48d2da91c3e7d4722bec493b9e67ca276a
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-22 18:10:53 +0100

    refactor: change event type

---
+++ src/types/events.ts
@@ -0,0 +1,17 @@
+export enum EventType {
+  // review
+  Review = "REVIEW",
+  // index  
+  IndexCreate = "INDEX:CREATE",
+  IndexUpdate = "INDEX:UPDATE",
+  IndexDelete = "INDEX:DELETE",
+  // session
+  SessionStart = "SESSION:START",
+  SessionEnd = "SESSION:END",
+}
+
+export type EventData<Entity> = {
+  entity_id: string,
+  created_at: Date,
+  entity: Entity,
+}
