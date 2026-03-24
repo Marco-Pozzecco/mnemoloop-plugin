f1b146e993abbb5f0b00cc4dc22123a2ee16b0cc
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-24 21:02:05 +0100

    feat: add adapter types

---
+++ src/types/adapters.ts
@@ -0,0 +1,18 @@
+import { IAdapter } from "@/interfaces/IAdapter";
+import { Flashcard, Stats, PluginSettings } from "@/schemas";
+
+export enum AdapterKey {
+  statistics = "statistics",
+  settings = "settings",
+  flashcard = "flashcard"
+}
+
+interface AdapterMap {
+  [AdapterKey.statistics]: IAdapter<Stats>;
+  [AdapterKey.settings]: IAdapter<PluginSettings>;
+  [AdapterKey.flashcard]: IAdapter<Flashcard>;
+}
+
+export type AdapterType<K extends AdapterKey = AdapterKey> = AdapterMap[K];
+
+export type Adapters = Map<AdapterKey, AdapterType>;
