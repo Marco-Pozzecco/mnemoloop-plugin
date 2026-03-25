fe9530660cb6982e02b437874a16ee3bee84ab76
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-25 11:03:51 +0100

    refactor: refactor code to follow the new event bus pattern

--- src/modules/review-items/FlashcardReviewItem.ts
+++ src/modules/review-items/FlashcardReviewItem.ts
@@ -1,10 +1,9 @@
 import { Flashcard } from "@/schemas";
 import { BaseReviewItem } from "./BaseReviewItem";
 import { IReviewEngine } from "@/interfaces/IReviewEngine";
-import { ISubscriber } from "@/interfaces/ISubscriber";
 
 export class FlashcardReviewItem extends BaseReviewItem<Flashcard> {
-  constructor(item: Flashcard, filepath: string, engine: IReviewEngine<Flashcard>, subscribers: ISubscriber<Flashcard>[]) {
-    super(item, filepath, engine, subscribers)
+  constructor(item: Flashcard, filepath: string, engine: IReviewEngine<Flashcard>) {
+    super(item, filepath, engine)
   }
 }
