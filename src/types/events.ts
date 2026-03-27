801e2052cb5bb530a14498b775f7636a920a9ddf
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-27 19:04:51 +0100

    feat: add review start and review end events

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,18 +1,58 @@
+import { Flashcard } from "@/schemas"
+import { Rating } from "ts-fsrs"
+
 export enum EventType {
   // review
-  Review = "REVIEW",
-  // index  
+  ReviewFlashcard = "REVIEW:FLASHCARD",
+  // index
+  IndexInit = "INDEX:INIT",
   IndexCreate = "INDEX:CREATE",
   IndexUpdate = "INDEX:UPDATE",
   IndexDelete = "INDEX:DELETE",
   // session
   SessionStart = "SESSION:START",
   SessionEnd = "SESSION:END",
+  // queue
+  QueueInit = "QUEUE:INIT",
 }
 
-export type EventData<Entity> = {
+export type EventData<Data> = {
   event_type: EventType
-  entity_id: string
   created_at: Date
-  entity: Entity
+  data: Data
 }
+
+export type QueueInitEntity = {
+  total_cards: number;
+  due_today: number;
+};
+
+export type IndexData = {}
+
+export type QueueInitEvent = EventData<QueueInitEntity>;
+export type IndexInitEvent = EventData<IndexData>;
+export type ReviewFlashcardEvent = EventData<Flashcard> & {
+  filepath: string,
+  rating: Rating
+};
+
+export type SessionStartEntity = {
+  session_id: string;
+  review_type: string;
+  start_time: number;
+};
+
+export type SessionEndEntity = {
+  session_id: string;
+  review_type: string;
+  date: string;
+  start_time: number;
+  end_time: number;
+  total_count: number;
+  correct_count: number;
+  incorrect_count: number;
+  duration_s: number;
+};
+
+export type SessionStartEvent = EventData<SessionStartEntity>;
+export type SessionEndEvent = EventData<SessionEndEntity>;
