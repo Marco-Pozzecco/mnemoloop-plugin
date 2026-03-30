e99b7ca59774fbfb8b55243f6c164d194681d04c
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-30 11:11:17 +0200

    refactor: refactor base class to allow for dynamic publishing of events in its child classes, then add init and save event firing to child classes

--- src/modules/adapters/StatisticsAdapter.ts
+++ src/modules/adapters/StatisticsAdapter.ts
@@ -1,43 +1,63 @@
-import { Plugin, TFile } from 'obsidian';
-import { BaseAdapter } from './BaseAdapter';
-import { StatsSchema, Stats } from '@/schemas/statistics';
+import { Stats, StatsSchema } from '@/schemas/statistics';
+import { AdapterEventsKeys, AdapterKey, AdapterStatsEvents } from '@/types/adapters';
 import { DEFAULT_STATISTICS } from '@/utils/constants';
 import { Logger } from '@/utils/Logger';
+import { Plugin } from 'obsidian';
+import { EventBus } from '../event-bus/EventBus';
+import { BaseAdapter } from './BaseAdapter';
 
-export class StatisticsAdapter extends BaseAdapter<Stats> {
-  private _path: string;
-  private _filename = 'statistics.json';
+export class StatisticsAdapter extends BaseAdapter<Stats, AdapterKey.statistics> {
+	private _path: string;
+	private _filename = 'statistics.json';
 
-  constructor(private plugin: Plugin) {
-    super(DEFAULT_STATISTICS);
-    this._path = `${plugin.manifest.dir}/${this._filename}`;
-  }
+	constructor(private plugin: Plugin) {
+		super(DEFAULT_STATISTICS, AdapterEventsKeys.statistics, 'stats');
+		this._path = `${plugin.manifest.dir}/${this._filename}`;
+	}
 
-  initialize: () => Promise<void> = async () => {
-    const file = await this.plugin.app.vault.adapter.read(this._path);
-    if (file) {
-      const json = JSON.parse(file);
-      try {
-        const index = this.validate(json);
-        this.set(index);
-      } catch (error) {
-        Logger.warn("Statistics file is corrupted. Resetting...")
-        this.save();
-      }
-    }
-    await this.save();
-  };
+	initialize: () => Promise<void> = async () => {
+		const file = await this.plugin.app.vault.adapter.read(this._path);
+		if (file) {
+			const json = JSON.parse(file);
+			try {
+				const index = this.validate(json);
+				this.set(index);
+			} catch (error) {
+				Logger.warn('Statistics file is corrupted. Resetting...');
+				this.save();
+			}
+		}
 
-  save: () => Promise<void> = async () => {
-    const file = await this.plugin.app.vault.adapter.exists(this._path);
-    if (file) {
-      await this.plugin.app.vault.adapter.write(this._path, JSON.stringify(this._data));
-    } else {
-      await this.plugin.app.vault.create(this._path, JSON.stringify(this._data));
-    }
-  };
+		const event: AdapterStatsEvents['init'] = {
+			event_type: this._eventTypes.init,
+			created_at: new Date(),
+			data: {
+				stats: this._data,
+			},
+		};
+		EventBus.instance.publish(event);
+	};
 
-  protected validate(data: Stats): Stats {
-    return StatsSchema.parse(data);
-  }
+	save: () => Promise<void> = async () => {
+		const file = await this.plugin.app.vault.adapter.exists(this._path);
+		if (file) {
+			await this.plugin.app.vault.adapter.write(this._path, JSON.stringify(this._data));
+		} else {
+			await this.plugin.app.vault.create(this._path, JSON.stringify(this._data));
+		}
+
+		const event: AdapterStatsEvents['save'] = {
+			event_type: this._eventTypes.save,
+			created_at: new Date(),
+			data: {
+				stats: this._data,
+				saved_at: new Date(),
+			},
+		};
+		EventBus.instance.publish(event);
+	};
+
+	protected validate(data: Stats): Stats {
+		return StatsSchema.parse(data);
+	}
 }
