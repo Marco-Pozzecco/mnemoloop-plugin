30ab8e3135b18e24821e72969627c0d366a0442e
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-30 11:09:40 +0200

    feat: add adpters event types

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,57 +1,71 @@
-import { Flashcard } from "@/schemas"
-import { Rating } from "ts-fsrs"
+import { Flashcard } from '@/schemas';
+import { Rating } from 'ts-fsrs';
+import { AdapterEventType } from './adapters';
 
-export enum EventType {
-  // review
-  ReviewFlashcard = "REVIEW:FLASHCARD",
-  // index
-  IndexInit = "INDEX:INIT",
-  IndexCreate = "INDEX:CREATE",
-  IndexUpdate = "INDEX:UPDATE",
-  IndexDelete = "INDEX:DELETE",
-  // session
-  SessionStart = "SESSION:START",
-  SessionEnd = "SESSION:END",
-  // queue
-  QueueInit = "QUEUE:INIT",
-}
+export const EventType = {
+	// review
+	ReviewFlashcard: 'REVIEW:FLASHCARD',
+	// index
+	IndexInit: 'INDEX:INIT',
+	IndexCreate: 'INDEX:CREATE',
+	IndexUpdate: 'INDEX:UPDATE',
+	IndexDelete: 'INDEX:DELETE',
+	// session
+	SessionStart: 'SESSION:START',
+	SessionEnd: 'SESSION:END',
+	// queue
+	QueueInit: 'QUEUE:INIT',
+	...AdapterEventType,
+} as const;
+
+export type EventType = (typeof EventType)[keyof typeof EventType];
 
 export type EventData<Data> = {
-  event_type: EventType
-  created_at: Date
-  data: Data
-}
-
-export type QueueInitEntity = {
-  total_cards: number;
-  due_today: number;
+	event_type: EventType;
+	created_at: Date;
+	data: Data;
 };
 
-export type IndexData = {}
+export type QueueInitEntity = {
+	total_cards: number;
+	due_today: number;
+};
+
+export type FlashcardIndexInitEntity = {
+	total_cards: number;
+	due_today: number;
+};
+
+export type FlashcardIndexInitEvent = EventData<FlashcardIndexInitEntity>;
+
+export type IndexInitEvent<Entity> = EventData<Entity>;
+export type IndexCreateEvent<Entity> = EventData<Entity>;
+export type IndexUpdateEvent<Entity> = EventData<Entity>;
+export type IndexDeleteEvent<Entity> = EventData<Entity>;
 
 export type QueueInitEvent = EventData<QueueInitEntity>;
-export type IndexInitEvent = EventData<IndexData>;
 export type ReviewFlashcardEvent = EventData<Flashcard> & {
-  filepath: string,
-  rating: Rating
+	filepath: string;
+	rating: Rating;
 };
 
 export type SessionStartEntity = {
-  session_id: string;
-  review_type: string;
-  start_time: number;
+	session_id: string;
+	review_type: string;
+	start_time: number;
 };
 
 export type SessionEndEntity = {
-  session_id: string;
-  review_type: string;
-  date: string;
-  start_time: number;
-  end_time: number;
-  total_count: number;
-  correct_count: number;
-  incorrect_count: number;
-  duration_s: number;
+	session_id: string;
+	review_type: string;
+	date: string;
+	start_time: number;
+	end_time: number;
+	total_count: number;
+	correct_count: number;
+	incorrect_count: number;
+	duration_s: number;
+	due_today: number;
 };
 
 export type SessionStartEvent = EventData<SessionStartEntity>;
