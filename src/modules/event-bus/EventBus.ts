4760d35ac13c7bb0c164acb49f95131ac45d7bbe
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-30 11:11:35 +0200

    feat: add debug logs to events fired

--- src/modules/event-bus/EventBus.ts
+++ src/modules/event-bus/EventBus.ts
@@ -1,32 +1,34 @@
-import { EventData } from '@/types/events'
-import { IEventBus } from '@/interfaces/IEventBus'
+import { EventData } from '@/types/events';
+import { IEventBus } from '@/interfaces/IEventBus';
+import { Logger } from '@/utils/Logger';
 
 export class EventBus implements IEventBus {
-  private static _instance: EventBus;
-  private _subscribers: Set<(event: EventData<unknown>) => void>;
+	private static _instance: EventBus;
+	private _subscribers: Set<(event: EventData<unknown>) => void>;
 
-  private constructor() {
-    this._subscribers = new Set()
-  }
+	private constructor() {
+		this._subscribers = new Set();
+	}
 
-  public static get instance(): EventBus {
-    if (!EventBus._instance) {
-      EventBus._instance = new EventBus()
-    }
-    return EventBus._instance
-  }
+	public static get instance(): EventBus {
+		if (!EventBus._instance) {
+			EventBus._instance = new EventBus();
+		}
+		return EventBus._instance;
+	}
 
-  public subscribe(callback: (event: EventData<unknown>) => void): void {
-    this._subscribers.add(callback)
-  }
+	public subscribe(callback: (event: EventData<unknown>) => void): void {
+		this._subscribers.add(callback);
+	}
 
-  public unsubscribe(callback: (event: EventData<unknown>) => void): void {
-    this._subscribers.delete(callback)
-  }
+	public unsubscribe(callback: (event: EventData<unknown>) => void): void {
+		this._subscribers.delete(callback);
+	}
 
-  public publish(event: EventData<unknown>): void {
-    this._subscribers.forEach((callback) => {
-      callback(event)
-    })
-  }
+	public publish(event: EventData<unknown>): void {
+		Logger.debug('Event fired:', event.event_type, event.data);
+		this._subscribers.forEach((callback) => {
+			callback(event);
+		});
+	}
 }
