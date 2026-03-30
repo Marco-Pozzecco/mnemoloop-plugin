e61bf1fb5150db8ce12aeaa39bdfd1b9de9b93d1
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-30 11:12:14 +0200

    chore: format

--- src/modules/event-listeners/EventListener.ts
+++ src/modules/event-listeners/EventListener.ts
@@ -1,22 +1,22 @@
-import { IEventListener } from "@/interfaces/IEventListener";
-import { EventBus } from "@/modules/event-bus/EventBus";
-import { EventData, EventType } from "@/types/events";
+import { IEventListener } from '@/interfaces/IEventListener';
+import { EventBus } from '@/modules/event-bus/EventBus';
+import { EventData, EventType } from '@/types/events';
 
 export abstract class EventListener implements IEventListener {
-  protected abstract readonly eventTypes: EventType[];
-  protected abstract process(event: EventData<unknown>): void;
+	protected abstract readonly eventTypes: EventType[];
+	protected abstract process(event: EventData<unknown>): void;
 
-  constructor() {
-    EventBus.instance.subscribe(this.handleEvent.bind(this));
-  }
+	constructor() {
+		EventBus.instance.subscribe(this.handleEvent.bind(this));
+	}
 
-  private handleEvent(event: EventData<unknown>): void {
-    if (this.eventTypes.includes(event.event_type)) {
-      this.process(event);
-    }
-  }
+	private handleEvent(event: EventData<unknown>): void {
+		if (this.eventTypes.includes(event.event_type)) {
+			this.process(event);
+		}
+	}
 
-  dispose(): void {
-    EventBus.instance.unsubscribe(this.handleEvent.bind(this));
-  }
+	dispose(): void {
+		EventBus.instance.unsubscribe(this.handleEvent.bind(this));
+	}
 }
