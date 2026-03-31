484b604f6b7fb40f89e6707b13f912bb7e094495
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-03-31 15:37:44 +0200

    feat: add recalc method to review queue

--- src/modules/review-items/BaseReviewItem.ts
+++ src/modules/review-items/BaseReviewItem.ts
@@ -1,30 +1,22 @@
-import { IReviewEngine } from "@/interfaces/IReviewEngine";
-import { IReviewItem } from "@/interfaces/IReviewItem";
-import { EventType } from "@/types/events";
-import { EventBus } from '@/modules/event-bus/EventBus'
+import { IReviewEngine } from '@/interfaces/IReviewEngine';
+import { IReviewItem } from '@/interfaces/IReviewItem';
 
-export abstract class BaseReviewItem<Entity extends Record<string, unknown>> implements IReviewItem<Entity> {
-  private _data: Entity;
-  private _filepath: string;
-  private _engine: IReviewEngine<Entity>
+export abstract class BaseReviewItem<
+	Entity extends Record<string, unknown>,
+> implements IReviewItem<Entity> {
+	protected _data: Entity;
+	protected _filepath: string;
+	protected _engine: IReviewEngine<Entity>;
 
-  constructor(data: Entity, filepath: string, engine: IReviewEngine<Entity>) {
-    this._data = data;
-    this._filepath = filepath;
-    this._engine = engine;
-  }
+	constructor(data: Entity, filepath: string, engine: IReviewEngine<Entity>) {
+		this._data = data;
+		this._filepath = filepath;
+		this._engine = engine;
+	}
 
-  get data() {
-    return this._data;
-  }
+	get data() {
+		return this._data;
+	}
 
-  review: <Score extends number>(score: Score) => void = (score) => {
-    this._data = this._engine.calculate(this._data, score);
-    EventBus.instance.publish({
-      event_type: EventType.Review,
-      entity_id: this._filepath,
-      created_at: new Date(),
-      entity: this._data
-    })
-  };
+	abstract review: <Score extends number>(score: Score) => void;
 }
--- src/modules/review-items/FlashcardReviewItem.ts
+++ src/modules/review-items/FlashcardReviewItem.ts
@@ -1,9 +1,23 @@
-import { Flashcard } from "@/schemas";
-import { BaseReviewItem } from "./BaseReviewItem";
-import { IReviewEngine } from "@/interfaces/IReviewEngine";
+import { IReviewEngine } from '@/interfaces/IReviewEngine';
+import { Flashcard } from '@/schemas';
+import { EventType, ReviewFlashcardEvent } from '@/types/events';
+import { EventBus } from '../event-bus/EventBus';
+import { BaseReviewItem } from './BaseReviewItem';
 
 export class FlashcardReviewItem extends BaseReviewItem<Flashcard> {
-  constructor(item: Flashcard, filepath: string, engine: IReviewEngine<Flashcard>) {
-    super(item, filepath, engine)
-  }
+	constructor(item: Flashcard, filepath: string, engine: IReviewEngine<Flashcard>) {
+		super(item, filepath, engine);
+	}
+
+	review: <Score extends number>(score: Score) => void = (score) => {
+		this._data = this._engine.calculate(this._data, score);
+		const event: ReviewFlashcardEvent = {
+			event_type: EventType.ReviewFlashcard,
+			filepath: this._filepath,
+			created_at: new Date(),
+			data: this._data,
+			rating: score,
+		};
+		EventBus.instance.publish(event);
+	};
 }
