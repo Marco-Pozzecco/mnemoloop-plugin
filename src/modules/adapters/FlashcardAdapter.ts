75e51611f76190d555ba5d5a5712faef43423379
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-02 15:38:51 +0200

    feat: allow partial recovery in case the json validation fails

--- src/modules/adapters/FlashcardAdapter.ts
+++ src/modules/adapters/FlashcardAdapter.ts
@@ -1,69 +1,37 @@
-import { FlashcardIndex, FlashcardIndexSchema } from '@/schemas';
+import { DEFAULT_FLASHCARD_INDEX, FlashcardIndex, FlashcardIndexSchema } from '@/schemas';
 import { BaseAdapter } from './BaseAdapter';
 import { Plugin, TFile } from 'obsidian';
-import { Logger } from '@/utils/Logger';
-import { AdapterEventsKeys, AdapterFlashcardEvents, AdapterKey } from '@/types/adapters';
-import { EventBus } from '../event-bus/EventBus';
-
-const DefaultData: FlashcardIndex = {
-	flashcards: [],
-	updated_at: null,
-};
+import { AdapterEventsKeys, AdapterKey } from '@/types/adapters';
 
 export class FlashcardAdapter extends BaseAdapter<FlashcardIndex, AdapterKey.flashcard> {
 	private _path: string;
 
 	constructor(private plugin: Plugin) {
-		super(DefaultData, AdapterEventsKeys.flashcard, 'flashcard_index');
+		super(
+			DEFAULT_FLASHCARD_INDEX,
+			AdapterEventsKeys.flashcard,
+			'flashcard_index',
+			FlashcardIndexSchema,
+		);
 		const dir = this.plugin.manifest.dir;
 		this._path = `${dir}/flashcard-index.json`;
 	}
 
-	initialize: () => Promise<void> = async () => {
+	protected async loadData(): Promise<unknown> {
 		const file = this.plugin.app.vault.getAbstractFileByPath(this._path);
 		if (file instanceof TFile) {
 			const contentStr = await this.plugin.app.vault.read(file);
-			const json = JSON.parse(contentStr);
-			try {
-				const index = this.validate(json);
-				this.set(index);
-			} catch (error) {
-				Logger.warn('Index file is corrupted. Resetting...');
-				this.save();
-			}
+			return JSON.parse(contentStr);
 		}
-		await this.save();
+		return this.defaultData;
+	}
 
-		const event: AdapterFlashcardEvents['init'] = {
-			event_type: AdapterEventsKeys.flashcard.init,
-			created_at: new Date(),
-			data: {
-				flashcard_index: this._data,
-			},
-		};
-		EventBus.instance.publish(event);
-	};
-
-	save: () => Promise<void> = async () => {
-		const file = await this.plugin.app.vault.adapter.exists(this._path);
-		if (file) {
-			await this.plugin.app.vault.adapter.write(this._path, JSON.stringify(this._data));
+	protected async saveData(data: FlashcardIndex): Promise<void> {
+		const fileExists = await this.plugin.app.vault.adapter.exists(this._path);
+		if (fileExists) {
+			await this.plugin.app.vault.adapter.write(this._path, JSON.stringify(data));
 		} else {
-			await this.plugin.app.vault.create(this._path, JSON.stringify(this._data));
+			await this.plugin.app.vault.create(this._path, JSON.stringify(data));
 		}
-
-		const event: AdapterFlashcardEvents['save'] = {
-			event_type: AdapterEventsKeys.flashcard.save,
-			created_at: new Date(),
-			data: {
-				flashcard_index: this._data,
-				saved_at: new Date(),
-			},
-		};
-		EventBus.instance.publish(event);
-	};
-
-	protected validate(data: FlashcardIndex) {
-		return FlashcardIndexSchema.parse(data);
 	}
 }
--- src/modules/adapters/SettingsAdapter.ts
+++ src/modules/adapters/SettingsAdapter.ts
@@ -1,38 +1,18 @@
 import { Plugin } from 'obsidian';
 import { BaseAdapter } from './BaseAdapter';
 import { PluginSettingsSchema, PluginSettings, DEFAULT_PLUGIN_SETTINGS } from '@/schemas/settings';
-import { AdapterEventsKeys, AdapterKey, AdapterSettingsEvents } from '@/types/adapters';
-import { EventBus } from '../event-bus/EventBus';
+import { AdapterEventsKeys, AdapterKey } from '@/types/adapters';
 
 export class SettingsAdapter extends BaseAdapter<PluginSettings, AdapterKey.settings> {
 	constructor(private plugin: Plugin) {
-		super(DEFAULT_PLUGIN_SETTINGS, AdapterEventsKeys.settings, 'settings');
+		super(DEFAULT_PLUGIN_SETTINGS, AdapterEventsKeys.settings, 'settings', PluginSettingsSchema);
 	}
 
-	initialize: () => Promise<void> = async () => {
-		const loaded = await this.plugin.loadData();
-		this._data = Object.assign({}, this.defaultData, loaded);
+	protected async loadData(): Promise<unknown> {
+		return await this.plugin.loadData();
+	}
 
-		const event: AdapterSettingsEvents['init'] = {
-			event_type: this._eventTypes.save,
-			created_at: new Date(),
-			data: { settings: this._data },
-		};
-		EventBus.instance.publish(event);
-	};
-
-	save: () => Promise<void> = async () => {
-		await this.plugin.saveData(this._data);
-
-		const event: AdapterSettingsEvents['save'] = {
-			event_type: this._eventTypes.save,
-			created_at: new Date(),
-			data: { settings: this._data, saved_at: new Date() },
-		};
-		EventBus.instance.publish(event);
-	};
-
-	protected validate(data: PluginSettings): PluginSettings {
-		return PluginSettingsSchema.parse(data);
+	protected async saveData(data: PluginSettings): Promise<void> {
+		await this.plugin.saveData(data);
 	}
 }
