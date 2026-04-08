04be24229a96dcfd52f7bf6715bcba13e34f9464
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-08 15:46:23 +0200

    refactor: pass parser to review item constructor

--- src/modules/review-queues/BaseReviewQueue.ts
+++ src/modules/review-queues/BaseReviewQueue.ts
@@ -1,20 +1,27 @@
 import { IIndexer } from '@/interfaces/IIndexer';
+import { IParser } from '@/interfaces/IParser';
 import { IReviewEngine } from '@/interfaces/IReviewEngine';
 import { IReviewItem } from '@/interfaces/IReviewItem';
 import { IReviewQueue } from '@/interfaces/IReviewQueue';
 
-export abstract class BaseReviewQueue<Entity> implements IReviewQueue<Entity> {
-	protected _engine: IReviewEngine<Entity>;
-	protected _index: IIndexer<Entity>;
+export abstract class BaseReviewQueue<
+	Entity extends EntityMetadata,
+	EntityMetadata,
+> implements IReviewQueue<Entity> {
+	protected _parser: IParser<Entity, EntityMetadata>;
+	protected _engine: IReviewEngine<EntityMetadata>;
+	protected _index: IIndexer<EntityMetadata>;
 	protected _items: IReviewItem<Entity>[] = [];
 	protected _position: number = 0;
-	protected _itemsQuery?: (entity: Entity) => boolean;
+	protected _itemsQuery?: (entity: EntityMetadata) => boolean;
 
 	constructor(
-		engine: IReviewEngine<Entity>,
-		indexer: IIndexer<Entity>,
-		query?: (entity: Entity) => boolean,
+		parser: IParser<Entity, EntityMetadata>,
+		engine: IReviewEngine<EntityMetadata>,
+		indexer: IIndexer<EntityMetadata>,
+		query?: (entity: EntityMetadata) => boolean,
 	) {
+		this._parser = parser;
 		this._engine = engine;
 		this._index = indexer;
 		this._itemsQuery = query;
