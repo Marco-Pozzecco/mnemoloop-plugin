e964286d558499851844b1645c9f7fc83c526a23
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-09 18:23:34 +0200

    refactor: cleanup the interface

--- src/modules/yaml-engines/BaseYamlEngine.ts
+++ src/modules/yaml-engines/BaseYamlEngine.ts
@@ -1,4 +1,4 @@
-import { IYamlEngine, YamlParseResult } from '@/interfaces/IYamlEngine';
+import { IYamlEngine } from '@/interfaces/IYamlEngine';
 import { EventBus } from '@/modules/event-bus/EventBus';
 import { EventData, EventType } from '@/types/events';
 import { ERROR_MESSAGES } from '@/utils/constants';
@@ -14,92 +14,65 @@ export abstract class BaseYamlEngine<T extends Record<string, unknown>> implemen
 	constructor(plugin: Plugin, schema: ZodType<T>) {
 		this._plugin = plugin;
 		this._schema = schema;
+
 		EventBus.instance.subscribe(this.handleEvent.bind(this));
 	}
 
 	abstract recover: (filepath: string) => Promise<void>;
 
-	async extractFromFile(filepath: string): Promise<YamlParseResult<T>> {
-		try {
-			const normalizedFilepath = normalizePath(filepath);
-			const file = this._plugin.app.vault.getFileByPath(normalizedFilepath);
+	async extractFmFromFile(filepath: string): Promise<T> {
+		const normalizedFilepath = normalizePath(filepath);
+		const content = await this._plugin.app.vault.adapter.read(normalizedFilepath);
 
-			if (!file) {
-				return {
-					error: 'file not found',
-					metadata: undefined,
-					success: false,
-				};
-			}
-
-			const metadata = this._plugin.app.metadataCache.getFileCache(file);
-
-			if (!metadata || !metadata.frontmatter) {
-				return {
-					metadata: undefined,
-					success: false,
-					error: ERROR_MESSAGES.INVALID_YAML,
-				};
-			}
-
-			const fm = metadata.frontmatter;
-			const entityMetadata = this.validate(fm);
-
-			return {
-				error: undefined,
-				success: true,
-				metadata: entityMetadata,
-			};
-		} catch (e) {
-			return {
-				metadata: undefined,
-				success: false,
-				error: e instanceof Error ? e.message : 'unknown error extracting yaml',
-			};
+		if (!content) {
+			throw new Error('file not found');
 		}
+
+		const { fm } = this.extractFmFromContent(content);
+		return fm;
 	}
 
-	extractFromContent: (content: string) => YamlParseResult<T> & { content: string } = (content) => {
-		try {
-			const match = content.match(this._yamlRegex);
+	extractFmFromContent(content: string): { fm: T; body: string } {
+		const match = content.match(this._yamlRegex);
 
-			if (!match) {
-				return {
-					success: false,
-					metadata: undefined,
-					error: ERROR_MESSAGES.INVALID_YAML,
-					content,
-				};
-			}
-
-			const yamlContent = match[0].replace(/^---\n/, '').replace(/\n---\n?$/, '');
-
-			const frontmatter = parseYaml(yamlContent);
-			const metadata = this.validate(frontmatter);
-			const cleanContent = this.removeFrontmatter(content);
-
-			return {
-				content: cleanContent,
-				metadata,
-				error: undefined,
-				success: true,
-			};
-		} catch (error) {
-			return {
-				content,
-				metadata: undefined,
-				error:
-					error instanceof Error ? error.message : 'unknown error extracting yaml from content',
-				success: false,
-			};
+		if (!match) {
+			throw new Error(ERROR_MESSAGES.INVALID_YAML);
 		}
-	};
+
+		const yamlContent = match[0].replace(/^---\n/, '').replace(/\n---\n?$/, '');
+
+		const frontmatter = parseYaml(yamlContent);
+		const metadata = this.validate(frontmatter);
+		const body = this.removeFrontmatter(content);
+
+		return { fm: metadata, body };
+	}
+
+	extractFmFromCache(filepath: string): T {
+		const normalizedFilepath = normalizePath(filepath);
+		const file = this._plugin.app.vault.getFileByPath(normalizedFilepath);
+
+		if (!file) {
+			throw new Error('file not found');
+		}
+
+		const metadata = this._plugin.app.metadataCache.getFileCache(file);
+
+		if (!metadata || !metadata.frontmatter) {
+			throw new Error(ERROR_MESSAGES.INVALID_YAML);
+		}
+
+		const fm = metadata.frontmatter;
+		const entityMetadata = this.validate(fm);
+
+		return entityMetadata;
+	}
 
 	async write(filepath: string, data: T): Promise<void> {
 		const normalizedFilepath = normalizePath(filepath);
 		const fullContent = await this._plugin.app.vault.adapter.read(normalizedFilepath);
 		const bodyContent = this.removeFrontmatter(fullContent);
-		const yamlFrontmatter = this.generateYamlString(data);
+		const yamlFrontmatter = this.encode(data);
 		const newContent = yamlFrontmatter + '\n' + bodyContent;
 		await this._plugin.app.vault.adapter.write(normalizedFilepath, newContent);
 	}
@@ -108,12 +81,12 @@ export abstract class BaseYamlEngine<T extends Record<string, unknown>> implemen
 		return this._schema.parse(data);
 	}
 
-	removeFrontmatter(content: string): string {
+	protected removeFrontmatter(content: string): string {
 		const match = content.match(this._yamlRegex);
 		return match ? content.slice(match[0].length) : content;
 	}
 
-	generateYamlString(data: T) {
+	encode(data: T): string {
 		const lines: string[] = [];
 
 		const entries = Object.entries(data);
@@ -131,6 +104,11 @@ export abstract class BaseYamlEngine<T extends Record<string, unknown>> implemen
 		lines.push('---');
 
 		return lines.join('\n');
+	}
+
+	decode(yaml: string): T {
+		const parsed = parseYaml(yaml);
+		return this.validate(parsed);
 	}
 
 	private handleEvent(event: EventData<unknown>): void {
