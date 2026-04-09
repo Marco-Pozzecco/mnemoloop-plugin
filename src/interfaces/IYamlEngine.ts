d48d19ce1597008ea1166ebacf39be812fcd3d21
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-09 18:23:34 +0200

    refactor: cleanup the interface

--- src/interfaces/IYamlEngine.ts
+++ src/interfaces/IYamlEngine.ts
@@ -1,23 +1,10 @@
 export interface IYamlEngine<Entity> {
-  extractFromFile: (filepath: string) => Promise<YamlParseResult<Entity>>;
-  extractFromContent: (content: string) => YamlParseResult<Entity> & { content: string };
-  generateYamlString: (data: Entity) => string;
-  write: (filepath: string, data: Entity) => Promise<void>;
-  recover: (filepath: string) => Promise<void>;
-  validate: (data: Record<string, unknown>) => Entity;
-  removeFrontmatter: (content: string) => string;
-}
-
-export type YamlParseResult<Entity> = YamlParseResultSuccess<Entity> | YamlParseResultError;
-
-interface YamlParseResultSuccess<Entity> {
-  success: true;
-  metadata: Entity;
-  error: undefined;
-}
-
-interface YamlParseResultError {
-  success: false;
-  metadata: undefined;
-  error: string;
+	encode(data: Entity): string;
+	decode(yaml: string): Entity;
+	extractFmFromFile(filepath: string): Promise<Entity>;
+	extractFmFromCache(filepath: string): Entity;
+	extractFmFromContent(content: string): { fm: Entity; body: string };
+	write(filepath: string, data: Entity): Promise<void>;
+	recover(filepath: string): Promise<void>;
+	validate(data: Record<string, unknown>): Entity;
 }
--- src/modules/yaml-engines/FlashcardYamlEngine.ts
+++ src/modules/yaml-engines/FlashcardYamlEngine.ts
@@ -1,5 +1,5 @@
 import { IYamlEngine } from '@/interfaces/IYamlEngine';
-import { DEFAULT_FLASHCARD_YAML, FlashcardMetadataSchema, FlashcardYaml } from '@/schemas';
+import { DEFAULT_FLASHCARD_YAML, FlashcardYaml, FlashcardYamlSchema } from '@/schemas';
 import { Plugin } from 'obsidian';
 import { v4 as uuid } from 'uuid';
 import { BaseYamlEngine } from './BaseYamlEngine';
@@ -9,18 +9,18 @@ export class FlashcardYamlEngine
 	implements IYamlEngine<FlashcardYaml>
 {
 	constructor(plugin: Plugin) {
-		super(plugin, FlashcardMetadataSchema);
+		super(plugin, FlashcardYamlSchema.strict());
 	}
 
 	recover: (filepath: string) => Promise<void> = async (filepath) => {
-		await this.write(filepath, this.generateDefaultYaml());
+		await this.write(filepath, this._generateYaml());
 	};
 
-	private generateDefaultYaml() {
+	private _generateYaml() {
 		const yaml: FlashcardYaml = {
 			uuid: uuid(),
 			...DEFAULT_FLASHCARD_YAML,
 		};
-		return yaml;
+		return this.validate(yaml);
 	}
 }
