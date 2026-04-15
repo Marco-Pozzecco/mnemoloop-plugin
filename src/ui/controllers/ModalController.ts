138ca0e49f4127d707dffbc8dc6ac5f3ba48d6ae
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-15 11:16:24 +0200

    refactor: pass cancel and confirm action functions via set instruction

--- src/ui/controllers/ModalController.ts
+++ src/ui/controllers/ModalController.ts
@@ -1,29 +1,45 @@
+import { Modal } from 'obsidian';
 import { ModalState, modalStore, ModalStore } from '../store/modal.store';
 
-export interface IModalController {
-	onConfirm(callback: () => void): void;
-	onCancel(callback: () => void): void;
-	onClose(callback: () => void): void;
+interface IModalController {
+	onConfirm(): void;
+	onCancel(): void;
+	onClose(): void;
 	readonly store: ModalStore;
 	readonly state: ModalState;
 }
 
 export class ModalController implements IModalController {
 	private _store: ModalStore = modalStore;
+	private _modal: Modal;
+	private _cancel: () => void = () => {};
+	private _confirm: () => void = () => {};
 
-	onCancel(callback: () => void): void {
-		callback();
-		this._store.close();
+	constructor(modal: Modal) {
+		this._modal = modal;
 	}
 
-	onClose(callback: () => void): void {
-		callback();
-		this._store.close();
+	set cancelAction(callback: () => void) {
+		this._cancel = callback;
 	}
 
-	onConfirm(callback: () => void): void {
-		callback();
+	set confirmAction(callback: () => void) {
+		this._confirm = callback;
+	}
+
+	onCancel(): void {
+		this._cancel();
+		this.onClose();
+	}
+
+	onClose(): void {
 		this._store.close();
+		this._modal.close();
+	}
+
+	onConfirm(): void {
+		this._confirm();
+		this.onClose();
 	}
 
 	get store() {
