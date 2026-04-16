e936a316e9272f69a41306eb71b23ea1d095e591
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-16 16:01:46 +0200

    feat: update listeners map

--- src/types/listeners.ts
+++ src/types/listeners.ts
@@ -1,14 +1,17 @@
 import { FileWatcherListener } from '@/modules/event-listeners/FileWatcherListener';
+import { FlashcardWriterProcess } from '@/modules/event-listeners/FlashcardWriterProcessor';
 import { StatisticsListener } from '@/modules/event-listeners/StatisticsListener';
 
 export enum ListenerKey {
 	statistics = 'statistics',
 	fileWatcher = 'fileWatcher',
+	flashcardProcessor = 'flashcardProcessor',
 }
 
 interface ListenerMap {
 	[ListenerKey.statistics]: StatisticsListener;
 	[ListenerKey.fileWatcher]: FileWatcherListener;
+	[ListenerKey.flashcardProcessor]: FlashcardWriterProcess;
 }
 
 export type ListenerType<K extends ListenerKey = ListenerKey> = ListenerMap[K];
