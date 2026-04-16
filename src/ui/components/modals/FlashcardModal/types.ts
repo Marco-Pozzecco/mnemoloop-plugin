1f98617197adc312bde0cc265befe1341ebb776b
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-16 15:54:40 +0200

    fix: reactiveness of modalstore

--- src/ui/components/modals/FlashcardModal/types.ts
+++ src/ui/components/modals/FlashcardModal/types.ts
@@ -1,8 +1,14 @@
-import { IModalController } from '@/ui/controllers/ModalController';
+import { ModalController } from '@/ui/controllers/ModalController';
 
-export default interface FlashcardModalProps {
-	controller: IModalController;
+export interface FlashcardModalProps {
+	controller: ModalController;
 	isLoading: boolean;
 	error: string | null;
-	initialData: unknown;
+}
+
+export interface FlashcardModalData {
+	front: string;
+	back: string;
+	deck: string;
+	filepath: string;
 }
