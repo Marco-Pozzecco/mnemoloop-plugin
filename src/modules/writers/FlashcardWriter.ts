6a7d8f338aa135dc373d50aac2026ab7e768bc78
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-16 15:59:38 +0200

    feat: implement flashcard writer concretion

---
+++ src/modules/writers/FlashcardWriter.ts
@@ -0,0 +1,38 @@
+import { IParser } from '@/interfaces/IParser';
+import {
+	Flashcard,
+	FlashcardContent,
+	FlashcardContentSchema,
+	FlashcardYaml,
+	FlashcardYamlSchema,
+} from '@/schemas';
+import { Plugin } from 'obsidian';
+import { FlashcardYamlEngine } from '../yaml-engines/FlashcardYamlEngine';
+import { BaseWriter } from './BaseWriter';
+
+export class FlashcardWriter extends BaseWriter<Flashcard, FlashcardYaml, FlashcardContent> {
+	private _parser: IParser<Flashcard, FlashcardYaml>;
+
+	constructor(plugin: Plugin, parser: IParser<Flashcard, FlashcardYaml>) {
+		super(plugin, new FlashcardYamlEngine(plugin));
+		this._parser = parser;
+	}
+
+	protected serializeBody(body: FlashcardContent): string {
+		const marker = this._parser.marker;
+		return `${body.front}\n\n${marker}\n\n${body.back}`;
+	}
+
+	protected deserializeBody(content: string): FlashcardContent {
+		const parsed = this._parser.parseContent(content);
+		return { front: parsed.entity.front, back: parsed.entity.back };
+	}
+
+	protected extractMetadata(entity: Flashcard): FlashcardYaml {
+		return FlashcardYamlSchema.parse(entity);
+	}
+
+	protected extractBody(entity: Flashcard): FlashcardContent {
+		return FlashcardContentSchema.parse(entity);
+	}
+}
