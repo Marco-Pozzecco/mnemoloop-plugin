45123836e2d65ff91a286a60f989b760ef944082
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-28 09:57:40 +0200

    feat: add app context and component lifecycle to Modal

--- src/ui/views/Modal/ModalView.ts
+++ src/ui/views/Modal/ModalView.ts
@@ -1,25 +1,32 @@
 import { ModalController } from '@/ui/controllers/ModalController';
-import { App, Modal } from 'obsidian';
+import { App, Component, Modal } from 'obsidian';
 import { mount, unmount } from 'svelte';
 import { default as ModalComponent } from './Modal.svelte';
 
 export class SvelteModal extends Modal {
 	protected svelteComponent: ReturnType<typeof mount> | null = null;
 	protected controller: ModalController;
+	protected className: string;
+	protected component: Component | null = null;
 
-	constructor(app: App) {
+	constructor(app: App, className: string) {
 		super(app);
 		this.controller = new ModalController(this);
+		this.className = className;
 	}
 
 	onOpen(): void {
 		this.contentEl.empty();
-		this.contentEl.addClass('ka-modal');
+		this.modalEl.addClass(this.className);
+		this.component = new Component();
+		this.component.load();
 
 		this.svelteComponent = mount(ModalComponent, {
 			target: this.contentEl,
 			props: {
 				controller: this.controller,
+				app: this.app,
+				component: this.component,
 			},
 		});
 	}
@@ -29,7 +36,11 @@ export class SvelteModal extends Modal {
 			unmount(this.svelteComponent);
 			this.svelteComponent = null;
 		}
+		if (this.component) {
+			this.component.unload();
+			this.component = null;
+		}
 		this.contentEl.empty();
-		this.contentEl.removeClass('ka-modal');
+		this.modalEl.removeClass(this.className);
 	}
 }
