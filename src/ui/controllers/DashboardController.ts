993b3e13e97dd58b6e4b7323cf57088c6f7e6588
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-30 15:27:31 +0200

    refactor: remove unused dependencies from DashboardController

--- src/ui/controllers/DashboardController.ts
+++ src/ui/controllers/DashboardController.ts
@@ -2,8 +2,7 @@ import { IReviewQueue } from '@/interfaces/IReviewQueue';
 import { EventBus, FlashcardReviewSessionStartEvent } from '@/modules/events';
 import { FlashcardReviewQueue } from '@/modules/review-queues/FlashcardReviewQueue';
 import { CardStatus, FlashcardMetadata } from '@/schemas';
-import { Indexes, IndexKey } from '@/types/indexes';
-import { ParserKey, Parsers } from '@/types/parsers';
+import { IndexKey } from '@/types/indexes';
 import { uiStore, UIStore } from '@/ui/store/ui.store';
 import { sessionStore, SessionStore } from '../store/session.store';
 
@@ -14,13 +13,8 @@ interface IDashboardController {
 export class DashboardController implements IDashboardController {
 	private _uiStore: UIStore = uiStore;
 	private _sessionStore: SessionStore = sessionStore;
-	private _indexes: Indexes;
-	private _parsers: Parsers;
 
-	constructor(indexes: Indexes, parsers: Parsers) {
-		this._indexes = indexes;
-		this._parsers = parsers;
-	}
+	constructor() {}
 
 	startReview: (type: IndexKey) => Promise<void> = async (type) => {
 		switch (type) {
@@ -32,21 +26,9 @@ export class DashboardController implements IDashboardController {
 	private async startFlashcardReview() {
 		this._uiStore.isLoading = true;
 
-		const index = this._indexes.get(IndexKey.flashcard);
-
-		if (!index) {
-			throw new Error(`index of kind::${IndexKey.flashcard} not initialized`);
-		}
-
-		const parser = this._parsers.get(ParserKey.flashcard);
-
-		if (!parser) {
-			throw new Error(`parser of kind::${ParserKey.flashcard} not initialized`);
-		}
-
 		const predicate = (entity: FlashcardMetadata) =>
 			entity.status === CardStatus.ACTIVE && new Date(entity.due) <= new Date();
-		const list = new FlashcardReviewQueue(parser, index, predicate);
+		const list = new FlashcardReviewQueue(predicate);
 
 		this._sessionStore.queue = list as IReviewQueue<unknown>;
 
