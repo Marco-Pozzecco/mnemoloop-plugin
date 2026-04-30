157721f4535cf5838e83335dbd8e99fe7dd9157d
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-30 11:56:39 +0200

    refactor: remove event system type definitions

--- src/types/adapters.ts
+++ src/types/adapters.ts
@@ -1,6 +1,5 @@
 import { IAdapter } from '@/interfaces/IAdapter';
 import { FlashcardIndex, PluginSettings, Stats } from '@/schemas';
-import { EventData } from './events';
 
 export enum AdapterKey {
 	statistics = 'statistics',
@@ -15,69 +14,3 @@ interface AdapterMap {
 }
 
 export type Adapters = Map<AdapterKey, AdapterMap[AdapterKey]>;
-
-type AdapterActions = 'set' | 'update' | 'reset' | 'save' | 'init';
-
-type AdapterDataSchema = {
-	settings: PluginSettings;
-	flashcard_index: FlashcardIndex;
-	stats: Stats;
-};
-
-export type AdapterDataKey = keyof AdapterDataSchema;
-
-type AdapterEntityEvents<K extends keyof AdapterDataSchema> = {
-	[A in AdapterActions]: A extends 'save'
-		? EventData<{ [P in K]: AdapterDataSchema[K] } & { saved_at: Date }>
-		: EventData<{ [P in K]: AdapterDataSchema[K] }>;
-};
-
-export type AdapterSettingsEvents = AdapterEntityEvents<'settings'>;
-export type AdapterFlashcardEvents = AdapterEntityEvents<'flashcard_index'>;
-export type AdapterStatsEvents = AdapterEntityEvents<'stats'>;
-
-export type AdapterEventsKeys = Record<AdapterKey, Record<AdapterActions, AdapterEventEnumValue>>;
-
-export const AdapterEventsKeys: AdapterEventsKeys = {
-	flashcard: generateKeys('FLASHCARD'),
-	settings: generateKeys('SETTINGS'),
-	statistics: generateKeys('STATISTICS'),
-};
-
-export type AdapterEventsOf<K extends keyof AdapterEventsKeys> = AdapterEventsKeys[K];
-
-type AdapterEventEnumKeys = `Adapter${Capitalize<AdapterKey>}${Capitalize<AdapterActions>}`;
-type AdapterEventEnumValue = `ADAPTER:${Uppercase<AdapterKey>}:${Uppercase<AdapterActions>}`;
-type AdapterEventEnum = Record<AdapterEventEnumKeys, AdapterEventEnumValue>;
-
-export const AdapterEventType = flattenAdapterKeys(AdapterEventsKeys);
-export type AdapterEventType = AdapterEventEnum[keyof typeof AdapterEventType];
-
-// utils
-function generateKeys(
-	entity: Uppercase<AdapterKey>,
-): Record<AdapterActions, AdapterEventEnumValue> {
-	return {
-		reset: `ADAPTER:${entity}:RESET`,
-		save: `ADAPTER:${entity}:SAVE`,
-		set: `ADAPTER:${entity}:SET`,
-		update: `ADAPTER:${entity}:UPDATE`,
-		init: `ADAPTER:${entity}:INIT`,
-	};
-}
-
-function flattenAdapterKeys(obj: typeof AdapterEventsKeys): AdapterEventEnum {
-	const result: Record<string, string> = {};
-	for (const [entity, actions] of Object.entries(obj)) {
-		for (const [action, key] of Object.entries(actions)) {
-			// e.g. "AdpterFlashcardSet" -> "ADAPTER:FLASHCARD:SET"
-			const enumKey = `Adapter${capitalize(entity)}${capitalize(action)}`;
-			result[enumKey] = key;
-		}
-	}
-	return result as AdapterEventEnum;
-}
-
-function capitalize(s: string): string {
-	return s.charAt(0).toUpperCase() + s.slice(1);
-}
--- src/types/indexes.ts
+++ src/types/indexes.ts
@@ -1,7 +1,5 @@
 import { IIndexer } from '@/interfaces/IIndexer';
 import { FlashcardMetadata } from '@/schemas';
-import { capitalize } from '@/utils/String';
-import { EventData } from './events';
 
 export enum IndexKey {
 	flashcard = 'flashcard',
@@ -14,62 +12,3 @@ interface IndexMap {
 export type IndexType<K extends IndexKey = IndexKey> = IndexMap[K];
 
 export type Indexes = Map<IndexKey, IndexType>;
-
-type OmittedActions = keyof Pick<
-	IIndexer<unknown>,
-	'index' | 'upsert' | 'getAll' | 'query' | 'get'
->;
-export type IndexActions = keyof Omit<IIndexer<unknown>, OmittedActions>;
-
-type IndexDataSchema = {
-	flashcard: {
-		flashcards: FlashcardMetadata[];
-		total: number;
-	};
-};
-
-export type IndexDataKey = keyof IndexDataSchema;
-
-type IndexEntityEvents<K extends keyof IndexDataSchema> = {
-	[A in IndexActions]: A extends 'save'
-		? EventData<IndexDataSchema[K] & { saved_at: Date }>
-		: EventData<IndexDataSchema[K]>;
-};
-
-export type IndexFlashcardEvents = IndexEntityEvents<'flashcard'>;
-
-export type IndexEventsKeys = Record<IndexKey, Record<IndexActions, IndexEventEnumValue>>;
-
-export const IndexEventsKeys: IndexEventsKeys = {
-	flashcard: generateKeys('FLASHCARD'),
-};
-
-export type IndexEventsOf<K extends keyof IndexEventsKeys> = IndexEventsKeys[K];
-
-type IndexEventEnumKeys = `Index${Capitalize<IndexKey>}${Capitalize<IndexActions>}`;
-type IndexEventEnumValue = `INDEX:${Uppercase<IndexKey>}:${Uppercase<IndexActions>}`;
-type IndexEventEnum = Record<IndexEventEnumKeys, IndexEventEnumValue>;
-
-export const IndexEventType = flattenIndexKeys(IndexEventsKeys);
-export type IndexEventType = IndexEventEnum[keyof typeof IndexEventType];
-
-// utils
-function generateKeys(entity: Uppercase<IndexKey>): Record<IndexActions, IndexEventEnumValue> {
-	return {
-		update: `INDEX:${entity}:UPDATE`,
-		create: `INDEX:${entity}:CREATE`,
-		delete: `INDEX:${entity}:DELETE`,
-		initialize: `INDEX:${entity}:INITIALIZE`,
-		save: `INDEX:${entity}:SAVE`,
-	};
-}
-function flattenIndexKeys(obj: IndexEventsKeys): IndexEventEnum {
-	const result: Record<string, string> = {};
-	for (const [entity, actions] of Object.entries(obj)) {
-		for (const [action, key] of Object.entries(actions)) {
-			const enumKey = `Index${capitalize(entity)}${capitalize(action)}`;
-			result[enumKey] = key;
-		}
-	}
-	return result as IndexEventEnum;
-}
