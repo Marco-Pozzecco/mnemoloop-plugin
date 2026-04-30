c5a6d41f28e0887534a0e97b7182c3e69e46632c
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-04-30 15:25:07 +0200

    feat: add flashcardParser processor type

--- src/types/processors.ts
+++ src/types/processors.ts
@@ -1,15 +1,18 @@
 import { FileWatcherProcessor } from '@/modules/events/processors/FileWatcherProcessor';
+import { FlashcardParserProcessor } from '@/modules/events/processors/FlashcardParserProcessor';
 import { FlashcardWriterProcessor } from '@/modules/events/processors/FlashcardWriterProcessor';
 import { StatisticsProcessor } from '@/modules/events/processors/StatisticsProcessor';
 
 export enum ProcessorKey {
 	fileWatcher = 'fileWatcher',
+	flashcardParser = 'flashcardParser',
 	flashcardWriter = 'flashcardWriter',
 	statistics = 'statistics',
 }
 
 interface ProcessorMap {
 	[ProcessorKey.fileWatcher]: FileWatcherProcessor;
+	[ProcessorKey.flashcardParser]: FlashcardParserProcessor;
 	[ProcessorKey.flashcardWriter]: FlashcardWriterProcessor;
 	[ProcessorKey.statistics]: StatisticsProcessor;
 }
