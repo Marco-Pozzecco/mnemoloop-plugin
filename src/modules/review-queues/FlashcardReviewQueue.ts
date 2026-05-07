3a13d23de9aeaaa020198c5006c28d0fff44b64f
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-05-07 17:23:14 +0200

    feat: add dispose method and refactor event handling

--- src/modules/review-queues/FlashcardReviewQueue.ts
+++ src/modules/review-queues/FlashcardReviewQueue.ts
@@ -7,38 +7,49 @@ import {
 import { FsrsEngine } from '../review-engines/FsrsEngine';
 import { FlashcardReviewItem } from '../review-items/FlashcardReviewItem';
 import { BaseReviewQueue } from './BaseReviewQueue';
+import { IEvent } from '@/interfaces/IEvent';
 
 export class FlashcardReviewQueue extends BaseReviewQueue<
 	Flashcard,
 	FlashcardMetadata,
 	FlashcardYaml
 > {
+	private _callback: (event: IEvent) => void;
+
 	constructor(predicate?: (entity: FlashcardMetadata) => boolean) {
 		const engine = new FsrsEngine();
 		super(engine, predicate);
 
-		EventBus.instance.subscribe((event) => {
+		this._callback = (event) => {
 			if (event.isType(FlashcardIndexQueryResponseEvent.type)) {
 				const data = (event as FlashcardIndexQueryResponseEvent).data;
-				const sortedData = this._engine.sort(data.flashcards);
+				const sortedData = this._engine.sort(data);
 				this._items = sortedData.map((f) => new FlashcardReviewItem(f.file, engine));
 			}
-		});
+		};
 
-		EventBus.instance.publish(
+		EventBus.instance.request(
 			new FlashcardIndexQueryRequestEvent({
 				predicate: predicate ?? (() => false),
 			}),
+			this._callback,
 		);
 	}
 
 	recalc(): void {
-		EventBus.instance.publish(
+		EventBus.instance.request(
 			new FlashcardIndexQueryRequestEvent({
-				predicate: this._itemsQuery ?? (() => true),
+				predicate: this._itemsQuery ?? (() => false),
 			}),
+			this._callback,
 		);
 
 		this._position = 0;
+	}
+
+	dispose(): void {
+		for (const item of this._items) {
+			item.dispose();
+		}
 	}
 }
