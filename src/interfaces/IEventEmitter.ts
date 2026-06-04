a23ed8663eacc37f57d568fa89ff024d033478e4
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-04 15:55:15 +0200

    feat: add event emitters

--- src/interfaces/IEventEmitter.ts
+++ src/interfaces/IEventEmitter.ts
@@ -1,3 +1,6 @@
+import { IEvent } from './IEvent';
+
 export interface IEventEmitter<T> {
 	emit: (action: T, data?: unknown) => void;
+	subscribe: (action: T, eventClass: new (...args: unknown[]) => IEvent) => void;
 }
