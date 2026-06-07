f06c5f68e37b4828bfeb2a5414f2b77404a5fb0a
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-07 20:39:05 +0200

    refactor: remove legacy processor system

--- src/modules/events/processors/index.ts
+++
@@ -1,6 +0,0 @@
-// Processor exports
-export { FileWatcherProcessor } from './FileWatcherProcessor';
-export { FlashcardParserProcessor } from './FlashcardParserProcessor';
-export { FlashcardWriterProcessor } from './FlashcardWriterProcessor';
-export { SettingsProcessor } from './SettingsProcessor';
-export { StatisticsProcessor } from './StatisticsProcessor';
