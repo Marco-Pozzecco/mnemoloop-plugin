c7c4036beeee882b439656c7a44156f36ecb78d9
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-07 15:00:04 +0200

    feat: add flashcard adapter event handlers

---
+++ src/modules/events/handlers/flashcard/adapter.ts
@@ -0,0 +1,64 @@
+import { IEventRegistryDependencies } from '@/interfaces/IEventRegistry';
+import { FlashcardAdapter } from '@/modules/adapters/FlashcardAdapter';
+import { AdapterKey } from '@/types/adapters';
+import { EventHandler } from '../../core/EventHandler';
+import {
+	FlashcardAdapterInitEvent,
+	FlashcardAdapterResetEvent,
+	FlashcardAdapterSaveEvent,
+	FlashcardAdapterSetEvent,
+	FlashcardAdapterUpdateEvent,
+} from '../../domains';
+
+export class FlashcardAdapterInitHandler extends EventHandler<FlashcardAdapterInitEvent> {
+	constructor(deps: IEventRegistryDependencies) {
+		super(deps);
+	}
+
+	async handle(_event: FlashcardAdapterInitEvent): Promise<void> {
+		const adapter = this._adapters.get(AdapterKey.flashcard)!;
+		await adapter.initialize();
+	}
+}
+
+export class FlashcardAdapterResetHandler extends EventHandler<FlashcardAdapterResetEvent> {
+	constructor(deps: IEventRegistryDependencies) {
+		super(deps);
+	}
+
+	async handle(_event: FlashcardAdapterResetEvent): Promise<void> {
+		await this._adapters.get(AdapterKey.flashcard)!.reset();
+	}
+}
+
+export class FlashcardAdapterSaveHandler extends EventHandler<FlashcardAdapterSaveEvent> {
+	constructor(deps: IEventRegistryDependencies) {
+		super(deps);
+	}
+
+	async handle(_event: FlashcardAdapterSaveEvent): Promise<void> {
+		await this._adapters.get(AdapterKey.flashcard)!.save();
+	}
+}
+
+export class FlashcardAdapterSetHandler extends EventHandler<FlashcardAdapterSetEvent> {
+	constructor(deps: IEventRegistryDependencies) {
+		super(deps);
+	}
+
+	async handle(event: FlashcardAdapterSetEvent): Promise<void> {
+		const adapter = this._adapters.get(AdapterKey.flashcard)! as FlashcardAdapter;
+		adapter.set(event.data);
+	}
+}
+
+export class FlashcardAdapterUpdateHandler extends EventHandler<FlashcardAdapterUpdateEvent> {
+	constructor(deps: IEventRegistryDependencies) {
+		super(deps);
+	}
+
+	async handle(event: FlashcardAdapterUpdateEvent): Promise<void> {
+		const adapter = this._adapters.get(AdapterKey.flashcard)! as FlashcardAdapter;
+		adapter.update(event.data);
+	}
+}
