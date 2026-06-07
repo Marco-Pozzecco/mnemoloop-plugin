9bdb47441dfc87a863ee28ce998bba2f5b16e38f
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-07 16:48:57 +0200

    feat: add writers support to EventHandler base class

--- src/modules/events/core/EventHandler.ts
+++ src/modules/events/core/EventHandler.ts
@@ -4,6 +4,7 @@ import { IEventRegistryDependencies } from '@/interfaces/IEventRegistry';
 import { Adapters } from '@/types/adapters';
 import { Indexes } from '@/types/indexes';
 import { Parsers } from '@/types/parsers';
+import { Writers } from '@/types/writers';
 import { Plugin } from 'obsidian';
 
 export abstract class EventHandler<E extends IEvent> implements IEventHandler {
@@ -11,12 +12,14 @@ export abstract class EventHandler<E extends IEvent> implements IEventHandler {
 	protected _parsers: Parsers;
 	protected _adapters: Adapters;
 	protected _plugin: Plugin;
+	protected _writers: Writers;
 
 	constructor(deps: IEventRegistryDependencies) {
 		this._indexers = deps.indexes;
 		this._parsers = deps.parsers;
 		this._adapters = deps.adapters;
 		this._plugin = deps.plugin;
+		this._writers = deps.writers;
 	}
 
 	abstract handle(_event: E): void | Promise<void>;
