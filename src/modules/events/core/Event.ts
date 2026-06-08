03dfc399b0d4ddd5b2cdc01a0e16297692de8def
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-08 15:08:26 +0200

    feat: add EventRequest and EventResponse base classes
    - Introduces EventRequest and EventResponse types for request/response events
    - Adds EventBus dependency to EventHandler via _bus
    - Changes static type pattern on Event base class for consistency

--- src/modules/events/core/Event.ts
+++ src/modules/events/core/Event.ts
@@ -1,13 +1,18 @@
 import { IEvent } from '@/interfaces/IEvent';
 import { v4 as uuid } from 'uuid';
 
+// Standard event base class
+// Used for events that do not produce a response
 export abstract class Event<TData> implements IEvent<TData> {
+	static type: string;
+
 	readonly id: string = uuid();
-	readonly type: string;
+	readonly type: string = Event.type;
 	readonly time: Date;
 	readonly data: TData;
 
 	constructor(type: string, data: TData) {
+		Event.type = type;
 		this.type = type;
 		this.time = new Date();
 		this.data = data;
@@ -15,16 +20,34 @@ export abstract class Event<TData> implements IEvent<TData> {
 
 	/** Check if this event matches a type */
 	isType(type: string): boolean {
-		return this.type === type;
+		return Event.type === type;
 	}
 
 	/** Serialize to JSON-friendly format */
 	toJSON() {
 		return {
-			type: this.type,
+			type: Event.type,
 			timestamp: this.time.toISOString(),
 			data: this.data,
 		};
+	}
+}
+
+// Event request base class
+// Used as requests for events that produce a response
+export abstract class EventRequest<TData> extends Event<TData> {
+	constructor(type: string, data: TData) {
+		type = `${type}:Request`;
+		super(type, data);
+	}
+}
+
+// Event response base class
+// The response to an event request
+export abstract class EventResponse<TData> extends Event<TData> {
+	constructor(type: string, data: TData) {
+		type = `${type}:Response`;
+		super(type, data);
 	}
 }
 
