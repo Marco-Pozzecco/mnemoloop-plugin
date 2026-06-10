d80f5445d9177a7832f13a1620ee168d14a92fd9
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-10 17:16:37 +0200

    refactor: rename FlashcardIndexInitializeEvent to FlashcardIndexInitEvent

--- src/modules/events/routes/flashcard/indexer.ts
+++ src/modules/events/routes/flashcard/indexer.ts
@@ -6,7 +6,7 @@ const router = new EventRouter();
 
 router.route(events.FlashcardIndexGetAllRequestEvent, handlers.FlashcardIndexGetAllHandler);
 router.route(events.FlashcardIndexGetRequestEvent, handlers.FlashcardIndexGetHandler);
-router.route(events.FlashcardIndexInitializeEvent, handlers.FlashcardIndexInitializeHandler);
+router.route(events.FlashcardIndexInitEvent, handlers.FlashcardIndexInitializeHandler);
 router.route(events.FlashcardIndexQueryRequestEvent, handlers.FlashcardIndexQueryHandler);
 router.route(events.FlashcardIndexSaveEvent, handlers.FlashcardIndexSaveHandler);
 router.route(events.FlashcardIndexUpdateRequestEvent, handlers.FlashcardIndexUpdateHandler);
