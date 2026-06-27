e8330a09e9d6be63f485565e4477ecf40e89720b
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-27 11:58:42 +0200

    refactor: narrow flashcard parser content response to omit stats

--- src/modules/events/domains/flashcard/parsers.ts
+++ src/modules/events/domains/flashcard/parsers.ts
@@ -18,42 +18,55 @@ type FlashcardParserParseEventData = {
 	filepath: string;
 };
 
-const FlashcardParserParseRequestEvent = EventFactory.createRequest<FlashcardParserParseEventData>(t.parse);
+const FlashcardParserParseRequestEvent = EventFactory.createRequest<FlashcardParserParseEventData>(
+	t.parse,
+);
 type FlashcardParserParseRequestEvent = IEvent<FlashcardParserParseEventData>;
 
-const FlashcardParserParseResponseEvent = EventFactory.createResponse<ParseResult<Flashcard>>(t.parse);
+const FlashcardParserParseResponseEvent = EventFactory.createResponse<ParseResult<Flashcard>>(
+	t.parse,
+);
 type FlashcardParserParseResponseEvent = IEvent<ParseResult<Flashcard>>;
 
 type FlashcardParserParseContentEventData = {
 	content: string;
 };
 
-const FlashcardParserParseContentRequestEvent = EventFactory.createRequest<FlashcardParserParseContentEventData>(t.parseContent);
+const FlashcardParserParseContentRequestEvent =
+	EventFactory.createRequest<FlashcardParserParseContentEventData>(t.parseContent);
 type FlashcardParserParseContentRequestEvent = IEvent<FlashcardParserParseContentEventData>;
 
 const FlashcardParserParseContentResponseEvent = EventFactory.createResponse<
-	Omit<ParseResult<Flashcard>, 'filepath'>
+	Omit<ParseResult<Flashcard>, 'filepath' | 'stats'>
 >(t.parseContent);
-type FlashcardParserParseContentResponseEvent = IEvent<Omit<ParseResult<Flashcard>, 'filepath'>>;
+type FlashcardParserParseContentResponseEvent = IEvent<
+	Omit<ParseResult<Flashcard>, 'filepath' | 'stats'>
+>;
 
 type FlashcardParserParseMetadataEventData = {
 	filepath: string;
 };
 
-const FlashcardParserParseMetadataRequestEvent = EventFactory.createRequest<FlashcardParserParseMetadataEventData>(t.parseMetadata);
+const FlashcardParserParseMetadataRequestEvent =
+	EventFactory.createRequest<FlashcardParserParseMetadataEventData>(t.parseMetadata);
 type FlashcardParserParseMetadataRequestEvent = IEvent<FlashcardParserParseMetadataEventData>;
 
-const FlashcardParserParseMetadataResponseEvent = EventFactory.createResponse<ParseResult<FlashcardYaml>>(t.parseMetadata);
+const FlashcardParserParseMetadataResponseEvent = EventFactory.createResponse<
+	ParseResult<FlashcardYaml>
+>(t.parseMetadata);
 type FlashcardParserParseMetadataResponseEvent = IEvent<ParseResult<FlashcardYaml>>;
 
 type FlashcardParserParseAllEventData = {
 	dirPath: string;
 };
 
-const FlashcardParserParseAllRequestEvent = EventFactory.createRequest<FlashcardParserParseAllEventData>(t.parseAll);
+const FlashcardParserParseAllRequestEvent =
+	EventFactory.createRequest<FlashcardParserParseAllEventData>(t.parseAll);
 type FlashcardParserParseAllRequestEvent = IEvent<FlashcardParserParseAllEventData>;
 
-const FlashcardParserParseAllResponseEvent = EventFactory.createResponse<ParseResult<FlashcardYaml>[]>(t.parseAll);
+const FlashcardParserParseAllResponseEvent = EventFactory.createResponse<
+	ParseResult<FlashcardYaml>[]
+>(t.parseAll);
 type FlashcardParserParseAllResponseEvent = IEvent<ParseResult<FlashcardYaml>[]>;
 
 export {
