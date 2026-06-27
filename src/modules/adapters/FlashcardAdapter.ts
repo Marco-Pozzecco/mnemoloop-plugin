8955685f7c1a2a1b84da735649290ba2af296d10
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-06-27 10:06:15 +0200

    refactor: simplify flashcard metadata flow in indexer and adapter

--- src/modules/adapters/FlashcardAdapter.ts
+++ src/modules/adapters/FlashcardAdapter.ts
@@ -1,31 +1,31 @@
 import { DEFAULT_FLASHCARD_INDEX, FlashcardIndex, FlashcardIndexSchema } from '@/schemas';
-import { Plugin, TFile } from 'obsidian';
+import { Plugin } from 'obsidian';
 import { BaseAdapter } from './BaseAdapter';
 
 export class FlashcardAdapter extends BaseAdapter<FlashcardIndex> {
 	private _path: string;
+	private _filename = 'flashcard-index.json';
 
 	constructor(private plugin: Plugin) {
 		super(DEFAULT_FLASHCARD_INDEX, FlashcardIndexSchema);
-		const dir = this.plugin.manifest.dir;
-		this._path = `${dir}/flashcard-index.json`;
+		this._path = `${plugin.manifest.dir}/${this._filename}`;
 	}
 
 	protected async loadData(): Promise<unknown> {
-		const file = this.plugin.app.vault.getAbstractFileByPath(this._path);
-		if (file instanceof TFile) {
-			const contentStr = await this.plugin.app.vault.read(file);
-			return JSON.parse(contentStr);
+		const content = await this.plugin.app.vault.adapter.read(this._path);
+		if (content) {
+			return JSON.parse(content);
 		}
 		return this.defaultData;
 	}
 
 	protected async saveData(data: FlashcardIndex): Promise<void> {
-		const fileExists = await this.plugin.app.vault.adapter.exists(this._path);
-		if (fileExists) {
-			await this.plugin.app.vault.adapter.write(this._path, JSON.stringify(data));
+		const exists = await this.plugin.app.vault.adapter.exists(this._path);
+		const serialized = JSON.stringify(data);
+		if (exists) {
+			await this.plugin.app.vault.adapter.write(this._path, serialized);
 		} else {
-			await this.plugin.app.vault.create(this._path, JSON.stringify(data));
+			await this.plugin.app.vault.create(this._path, serialized);
 		}
 	}
 }
