e4527fd832bfa6125199304088e495b64e5bcdf0
Marco-Pozzecco <pozzecco.marco@gmail.com>
2026-07-28 12:47:37 +0200

    refactor: consolidate flashcard commands into modal-driven commands
    Replace four scattered commands (palette empty, editor-menu
    selection, file-menu create, file-menu AI-generate) with
    two focused modal-driven commands.

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1,32 +1,25 @@
 import { OpenDashboardCommand } from '@/modules/commands/palette/OpenDashboardCommand';
-import { CreateEmptyFlashcardCommand } from '@/modules/commands/palette/CreateEmptyFlashcardCommand';
 import { SetAllFlashcardsDueNowCommand } from '@/modules/commands/palette/SetAllFlashcardsDueNowCommand';
 import { DebugAddTestFlashcardCommand } from '@/modules/commands/palette/DebugAddTestFlashcardsCommand';
-import { GenerateFromSelectionCommand } from '@/modules/commands/editor-menu/GenerateFromSelectionCommand';
-import { AIGenerateFromFileCommand } from '@/modules/commands/file-menu/AIGenerateFromFileCommand';
-import { CreateFlashcardFromFileCommand } from '@/modules/commands';
+import { CreateFlashcardModalCommand } from '@/modules/commands/palette/CreateFlashcardModalCommand';
+import { CreateFlashcardFromFileModalCommand } from '@/modules/commands/file-menu/CreateFlashcardFromFileModalCommand';
 
 export enum CommandKey {
 	// palette
 	openDashboard = 'openDashboard',
-	createEmptyFlashcard = 'createEmptyFlashcard',
 	setAllFlashcardsDueNow = 'setAllFlashcardsDueNow',
 	debugAddTestFlashcards = 'debugAddTestFlashcards',
+	createFlashcardModal = 'createFlashcardModal',
 	// file menu
-	generateFromFile = 'generateFromFile',
-	createFlashcardFromFile = 'createFlashcardFromFile',
-	// editor menu
-	generateFromSelection = 'generateFromSelection',
+	createFlashcardFromFileModal = 'createFlashcardFromFileModal',
 }
 
 interface CommandMap {
 	[CommandKey.openDashboard]: OpenDashboardCommand;
-	[CommandKey.createEmptyFlashcard]: CreateEmptyFlashcardCommand;
 	[CommandKey.setAllFlashcardsDueNow]: SetAllFlashcardsDueNowCommand;
 	[CommandKey.debugAddTestFlashcards]: DebugAddTestFlashcardCommand;
-	[CommandKey.generateFromSelection]: GenerateFromSelectionCommand;
-	[CommandKey.generateFromFile]: AIGenerateFromFileCommand;
-	[CommandKey.createFlashcardFromFile]: CreateFlashcardFromFileCommand;
+	[CommandKey.createFlashcardModal]: CreateFlashcardModalCommand;
+	[CommandKey.createFlashcardFromFileModal]: CreateFlashcardFromFileModalCommand;
 }
 
 export type Commands = Map<CommandKey, CommandMap[CommandKey]>;
